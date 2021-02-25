2b63ed94fb0149e924b682fe6bbeb577eea42d4a
ekkapotc <37437364+ekkapotc@users.noreply.github.com>
2021-02-25 12:08:05 +0700

    Create ImplicitBarrier.cpp

---
+++ LiveDemo/OpenMP/ImplicitBarrier/ImplicitBarrier.cpp
@@ -0,0 +1,26 @@
+#include <omp.h>
+#include <iostream>
+
+int main(int argc,char ** argv){
+ 
+    int i,n=128;
+    double a[n],b[n];
+    
+    
+    #pragma omp parlllel shared(n,a,b), private(i)
+    {
+        #pragma omp for
+        for(i=0 ; i<n ; i++)
+        {
+            a[i] = static_cast<double>(i);
+        }/*---Implicit barrier---*/
+        
+        #pragma omp for
+        for(i=0 ; i<n ; i++)
+        {
+            b[i] = a[i]*static_cast<double>(i);
+        }/*---Implicit barrier---*/
+    }/*---End of parallel region---*/
+    
+    return 0;
+}
