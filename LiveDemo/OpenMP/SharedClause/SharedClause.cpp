85d285ca360329499193a3f25a9a8ba60b1c91ae
ekkapotc <37437364+ekkapotc@users.noreply.github.com>
2021-02-25 12:01:36 +0700

    Add files via upload

--- LiveDemo/OpenMP/SharedClause/SharedClause.cpp
+++ LiveDemo/OpenMP/SharedClause/SharedClause.cpp
@@ -1,1 +1,22 @@
+#include <omp.h>
+#include <iostream>
 
+#define N 1024
+
+int main(int argc,char** argv){
+  
+  double a[1024];
+
+  for(int i=0; i<N;i++)
+	a[i] = static_cast<double>(i*i);
+
+  #pragma omp parallel for shared(a)
+  for(int i=0;i<N;i++)
+  {
+      
+	a[i] += static_cast<double>(i);
+      
+  }/*--- End of parallel for ---*/
+
+  return 0;
+}
