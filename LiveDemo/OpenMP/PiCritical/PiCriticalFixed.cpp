148984104617148b04fe5446b86f7a266adfc85d
ekkapotc <37437364+ekkapotc@users.noreply.github.com>
2021-02-27 02:02:19 +0700

    Update PiCriticalFixed.cpp

--- LiveDemo/OpenMP/PiCritical/PiCriticalFixed.cpp
+++ LiveDemo/OpenMP/PiCritical/PiCriticalFixed.cpp
@@ -5,13 +5,16 @@
 #include <iomanip>
 #include <omp.h>
 
+
 #define NUMITERATIONS 1000000000  
+
 
 int main(int argc, char *argv[])
 {
     double totalCount = NUMITERATIONS;
     double totalHits = 0;
     unsigned int seedVal;
+    
     
     std::cout << "\tThe number of processors available      = " << omp_get_num_procs() << std::endl;
     std::cout << "\tThe number of threads available         = " << omp_get_max_threads() << std::endl;;
@@ -22,7 +25,8 @@ int main(int argc, char *argv[])
     #pragma omp parallel default(none) shared(totalHits) private(seedVal) 
     {
         seedVal = omp_get_thread_num();//Each thread seeds with its own thread id.
-        double localTotalHits = 0.0;
+        
+        double localTotalHits = 0.0;//local sum
         
         #pragma omp for 
         for(int i=0 ; i<NUMITERATIONS ; i++)
@@ -33,21 +37,24 @@ int main(int argc, char *argv[])
             double result = std::sqrt((x*x) + (y*y));
                 
             if(result<1.0){
-                localTotalHits += 1.0;//check if the generated value is inside a unit circle.
-            } 
+               localTotalHits += 1.0;//check if the generated value is inside a unit circle.
+            }
+            
         }    
-       
+        
         #pragma omp critical(update_total_hits)
         {//critical section
-            totalHits += localTotalHits;
+            totalHits += localTotalHits;//update the total hits from the local sum 
         }
     }
    
+
     double runtime = omp_get_wtime() - wall_time;
     
     double pi = 4.0*totalHits/totalCount;
    
     std::cout << std::fixed;
+
     std::cout << std::setprecision(6);
     
     std::cout << "The value of Pi is approximately " << pi << "." << std::endl;
